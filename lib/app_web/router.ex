458fe29da01dede1f171a028da23bd55d8f2a78e
vishal <vishal.auroville@gmail.com>
2023-04-17 11:33:12 +0530

    first commit

---
+++ lib/app_web/router.ex
@@ -0,0 +1,28 @@
+defmodule AppWeb.Router do
+  use AppWeb, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_live_flash
+    plug :put_root_layout, {AppWeb.Layouts, :root}
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", AppWeb do
+    pipe_through :browser
+
+    get "/", PageController, :home
+    resources "/items", ItemController
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", AppWeb do
+  #   pipe_through :api
+  # end
+end
